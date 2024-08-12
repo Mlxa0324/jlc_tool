660fe6b2b31027a26132a5b65bb785fa1cdd9ec1
thisY <14195457+thisy0312@user.noreply.gitee.com>
2024-08-12 16:09:23 +0800

    1

--- js/嘉立创购物车辅助工具.js
+++ js/嘉立创购物车辅助工具.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         嘉立创购物车辅助工具
 // @namespace    http://tampermonkey.net/
-// @version      1.9.6
+// @version      1.9.7
 // @description  嘉立创购物车辅助增强工具 包含：手动领券、自动领券、小窗显示优惠券领取状态、一键分享BOM、一键锁定/释放商品、一键换仓、一键选仓、搜索页优惠券新老用户高亮。
 // @author       Lx
 // @match        https://cart.szlcsc.com/cart/display.html**
