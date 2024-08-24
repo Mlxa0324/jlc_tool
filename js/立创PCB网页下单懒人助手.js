97a1b7ed055134b0a89f8dc9b6af9f8d9d93f48a
maLix <mlx950324@163.com>
2024-08-24 23:34:38 +0800

    1

--- js/立创PCB网页下单懒人助手.js
+++ js/立创PCB网页下单懒人助手.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         立创PCB网页下单懒人助手
 // @namespace    http://tampermonkey.net/
-// @version      1.1.4
+// @version      1.1.5
 // @description  PCB网页下单懒人助手
 // @author       Lx
 // @match        https://www.jlc.com/newOrder**
@@ -13,9 +13,9 @@
 
 
 /**
-* 延迟方法
-* @param time
-*/
+ * 延迟方法
+ * @param time
+ */
 const awaitTime = (time) => {
     return new Promise((resolve, reject) => {
         let tid = setTimeout(() => {
@@ -51,17 +51,16 @@ const timerFunc = (taskFunc, successBoolFunc, timeout = 1000) => {
             clearInterval(_timer_);
             _timer_ = null;
         } else {
-            taskFunc && taskFunc();
+            runIgnoreError(() => taskFunc && taskFunc());
         }
     }, timeout);
 }
 
-const start = async () => {
+const start = async() => {
 
-     runIgnoreError(() => {
+    runIgnoreError(() => {
         // 长宽为空的话,重新加载页面
         timerFunc(() => {
-            debugger
             $('#pcbLengthInput').val('10');
             $('#pcbWidthInput').val('10');
         }, () => $('#pcbLengthInput').val() > 0, 1000);
@@ -76,13 +75,13 @@ const start = async () => {
         timerFunc(() => $('#confirmProductionFile_no').click(), () => $('#confirmProductionFile_no').hasClass('checked'), 1000);
     })
 
-    runIgnoreError(async () => {
-        timerFunc(async () => {
+    runIgnoreError(async() => {
+        timerFunc(async() => {
             // 数量选择
             if ($('#pcbNumberModal').length === 0) {
                 $('#pcbNumber input[placeholder="数量"]').click();
                 await awaitTime(1000 * 0.5)
-                $('#pcbNumberModal li.numItem button:not([class*=checked]').each(function(){
+                $('#pcbNumberModal li.numItem button:not([class*=checked]').each(function() {
                     if ($(this).text().replace(/[ \n]/g, '') === "5") {
                         $(this).click()
                     }
@@ -91,32 +90,26 @@ const start = async () => {
         }, () => $('#pcbNumber input[placeholder="数量"]').val() > 0, 1000);
     })
 
-
-    runIgnoreError(async () => {
+    runIgnoreError(async() => {
         // 包装 空白盒子
         timerFunc(() => $('#packType_white').click(), () => $('#packType_white').hasClass('checked'), 1000);
     })
 
 
-    runIgnoreError(async () => {
+    runIgnoreError(async() => {
         // 出货方式 单片资料单片出货
         timerFunc(() => $('#stencilType_one').click(), () => $('#stencilType_one').hasClass('checked'), 1000);
     })
 
-
     runIgnoreError(() => {
         // 默认 哑黑色 颜色
-        if (!$('#adornColor_black').hasClass('checked')) {
-            $('#adornColor_black').click()
-        }
+        timerFunc(() => $('#adornColor_black').click(), () => $('#adornColor_black').hasClass('checked'), 1000);
     })
-
 
     runIgnoreError(() => {
         // 是否SMT贴片 不需要
-        timerFunc(() => $('#isNeedOrderSMT_no').click(), () => $('#isNeedOrderSMT_no').hasClass('checked'), 2500);
+        timerFunc(() => $('#isNeedOrderSMT_no').click(), () => $('#isNeedOrderSMT_no').hasClass('checked'), 2000);
     })
-
 
     runIgnoreError(() => {
         // 是否开钢网 不需要
@@ -125,31 +118,27 @@ const start = async () => {
         }
     })
 
-
-    runIgnoreError(async () => {
+    runIgnoreError(async() => {
         // 个人/普通电子发票
         if (!$('#isinvoiceFlag_no_8').hasClass('checked')) {
             $('#isinvoiceFlag_no_8').click()
         }
         await awaitTime(1000 * 0.5)
-        // 这里是发票选择人
+            // 这里是发票选择人
         if (!$('span:contains(选择开票资料)').hasClass('checked')) {
             $('span:contains(选择开票资料)').click()
         }
     })
-
 
     runIgnoreError(() => {
         // 手动确认订单
         timerFunc(() => $('#isConfirmStatus_no').click(), () => $('#isConfirmStatus_no').hasClass('checked'), 1000);
     })
 
-
     runIgnoreError(() => {
         // 不同交期订单一起发货(省运费)
         timerFunc(() => $('#bingdDelivery_2').click(), () => $('#bingdDelivery_2').hasClass('checked'), 1000);
     })
-
 
     runIgnoreError(() => {
         // 快递
@@ -158,30 +147,37 @@ const start = async () => {
         }
     })
 
-
-    runIgnoreError(async () => {
+    runIgnoreError(async() => {
         await awaitTime(1000 * 3)
-        // 不同交期订单一起发货(省运费)
+            // 不同交期订单一起发货(省运费)
         if (!$('#isNeedOrderSMT_confirm_no').hasClass('checked')) {
             $('#isNeedOrderSMT_confirm_no').click()
         }
     })
 
-
-    runIgnoreError(async () => {
-        timerFunc(async () => {
+    runIgnoreError(async() => {
+        timerFunc(async() => {
             $('div#banshangjiabiaozhi_1:contains("加嘉立创客编") button').click();
-            await awaitTime(1000 * 0.5)
+            await awaitTime(1000 * 1)
             $("div#biaozhiweizhi_1 button").click();
-            await awaitTime(1000 * 0.5)
+            await awaitTime(1000 * 1)
             $('div.el-dialog.jlc-modal[role="dialog"][aria-label="加客编"] button.jlc-button:contains("确定")').click();
-            await awaitTime(1000 * 0.5)
+            await awaitTime(1000 * 1)
             $('div.el-dialog[role="dialog"][aria-label="温馨提示"] button[type="button"]:contains("不确认生产稿")').click();
-        }, () => $('div#banshangjiabiaozhi_1:contains("加嘉立创客编") button').hasClass('checked'), 1000);
+        }, () => $('div#banshangjiabiaozhi_1:contains("加嘉立创客编") button').hasClass('checked') && $('div.selectItemMaxWidth span:contains("指定位置添加")').length > 0, 1000);
     })
 
+    runIgnoreError(async() => {
+        setInterval(async() => {
+            var returnBool = $('iframe#editInvoiceListIfr').contents().find('div.el-dialog.jlc-modal[role="dialog"]:contains("开票资料")').is(':visible');
+            if (returnBool) {
+                await awaitTime(1000 * 0.5);
+                $('iframe#editInvoiceListIfr').contents().find('div.el-dialog.jlc-modal[role="dialog"]:contains("开票资料") button.jlc-button:contains("确定")').click();
+                await awaitTime(1000 * 0.5);
+                $('div.el-dialog[role="dialog"][aria-label="温馨提示"] button[type="button"]:contains("确认选择")').click();
+            }
+        }, 1500);
 
-    runIgnoreError(async () => {
         // 发票选择第一个 确认
         if (!$(window.frames[2].document).find(".cancelPadding .hideLable").hasClass('is-checked')) {
 
@@ -195,14 +191,14 @@ const start = async () => {
         }
     })
 
-    runIgnoreError(async () => {
-        timerFunc(async () => {
+    runIgnoreError(async() => {
+        timerFunc(async() => {
             // 选择优惠券按钮
             $('.selectCoupon').click()
-            // 默认选择第一张优惠券
+                // 默认选择第一张优惠券
             await awaitTime(1000 * 0.5)
             $('#collarCouponId_0').click()
-            // 提交优惠券
+                // 提交优惠券
             await awaitTime(1000 * 0.5)
             $("button:contains(使用优惠券):eq(1)").click();
         }, () => $('div#useCollarCoupon:contains("已选择")').length > 0, 2000);
